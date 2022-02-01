c1bfd47ee69bd1a8551f3db8b5dec578c0f18d17
Marshal-S <921871381@qq.com>
2022-02-01 14:30:45 +0800

    Initial Commit

---
+++ JSONDecodeDemo/JSONDecodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDecodeDemoApp.swift
+//  JSONDecodeDemo
+//
+//  Created by 李帅 on 2022/2/1.
+//
+
+import SwiftUI
+
+@main
+struct JSONDecodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
